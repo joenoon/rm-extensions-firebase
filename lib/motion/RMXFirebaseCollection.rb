d99944a750360c23411f11f2031ba10eecf309b1
Joe Noon <joenoon@gmail.com>
2014-08-21 08:20:32 -0700

    rewrite RMXFirebaseCollection, no longer mirrors Firebase events, has a higher level interface only dealing with models

--- lib/motion/RMXFirebaseCollection.rb
+++ lib/motion/RMXFirebaseCollection.rb
@@ -2,7 +2,12 @@ class RMXFirebaseCollection
 
   include RMXCommonMethods
 
-  attr_reader :ref, :snaps, :transformations_table, :cancel_error
+  attr_reader :ref, :snaps, :cancel_error
+
+  # public, override required
+  def transform(snap)
+    raise "#{className}#transform(snap): override to return a RMXFirebaseModel based on the snap"
+  end
 
   # public
   def ready?
@@ -14,21 +19,12 @@ class RMXFirebaseCollection
     @state == :cancelled
   end
 
-  # overridable, by default just returns the snap
-  def transform(snap)
-    snap
+  # public
+  def changed?
+    @state
   end
 
-  def transformed_async(&block)
-    transformed(:async, &block)
-  end
-
-  # public, completes with ready transformations
-  def transformed(queue=nil, &block)
-    transformed(queue, nil, &block)
-  end
-
-  # public, completes with ready transformations
+  # public, completes with changed transformations
   def transformed(queue=nil, pager=nil, &block)
     RMXFirebase::QUEUE.barrier_async do
       starting_at = pager && pager.starting_at
@@ -68,198 +64,191 @@ class RMXFirebaseCollection
       end
       if first && last
         i = first
-        while i <= last
+        while i < last
           break unless snap = @snaps[i]
-          break unless model = store_transform(snap)
+          break unless model = transform(snap)
           items << model
           i += 1
         end
       end
+      if last_snaps = @last_snaps
+        removed = (last_snaps - @snaps).map { |x| transform(x) }
+        added = (@snaps - last_snaps).map { |x| transform(x) }
+        if removed.size > 0
+          RMXFirebaseBatch.new(removed).once(:async) do |removed_models|
+            removed_models.each do |removed_model|
+              RMX(self).trigger(:removed_model, removed_model)
+            end
+          end
+        end
+        if added.size > 0
+          RMXFirebaseBatch.new(added).once(:async) do |added_models|
+            added_models.each do |added_model|
+              RMX(self).trigger(:added_model, added_model)
+            end
+          end
+        end
+      end
+      @last_snaps = @snaps.dup
       if order == :desc
         items = items.reverse
       end
-      if (snap = items.first) && snap.is_a?(RMXFirebaseModel)
-        RMXFirebaseBatch.new(items).once(queue, &block)
-      else
-        RMXFirebase.block_on_queue(queue, items, &block)
-      end
+      RMXFirebaseBatch.new(items).once(queue, &block)
     end
     self
   end
 
-  # completes with `self` once, when the collection is ready.
+  # completes with `self` once, when the collection is changed.
   # retains `self` and the sender until complete
   def once(queue=nil, &block)
     RMXFirebase::QUEUE.barrier_async do
-      if ready?
+      if changed?
         RMXFirebase.block_on_queue(queue, self, &block)
       else
-        RMX(self).once(:ready, :strong => true, :queue => queue, &block)
+        RMX(self).once(:changed, :strong => true, :queue => queue, &block)
       end
     end
     self
   end
 
-  def once_models(queue=nil, &block)
-    once(:async) do |collection|
-      collection.transformed(&block)
+  # completes with `self` immediately if changed, and every time the collection changes.
+  # does not retain `self` or the sender.
+  # returns an "unbinder" that can be called to stop listening.
+  def always(queue=nil, &block)
+    if changed?
+      RMXFirebase.block_on_queue(queue, self, &block)
     end
+    RMX(self).on(:changed, :queue => queue, &block)
   end
 
+  # completes with `self` every time the collection changes.
+  # does not retain `self` or the sender.
+  # returns an "unbinder" that can be called to stop listening.
+  def changed(queue=nil, &block)
+    RMX(self).on(:changed, :queue => queue, &block)
+  end
+
+  # completes with `models` once, when the collection is changed.
+  # takes optional pager.
+  # retains `self` and the sender until complete
   def once_models(queue=nil, pager=nil, &block)
     once_canceller = once(:async) do |collection|
       collection.transformed(queue, pager, &block)
     end
-    pager_canceller = RMX(pager).on(:changed) do
-      changed!
+    pager_canceller = if pager
+      RMX(pager).on(:changed) do
+        changed!
+      end
     end
     off_block = proc do
       once_canceller.call
-      pager_canceller.call
+      if pager_canceller
+        pager_canceller.call
+      end
     end
     off_block
   end
 
-  # completes with `self` immediately if ready, and every time the collection :ready fires.
+  # completes with `models` immediately if changed, and every time the collection changes.
   # does not retain `self` or the sender.
+  # takes optional pager.
   # returns an "unbinder" that can be called to stop listening.
-  def always(queue=nil, &block)
-    return false if cancelled?
-    if ready?
-      RMXFirebase.block_on_queue(queue, self, &block)
-    end
-    RMX(self).on(:ready, :queue => queue, &block)
-  end
-
-  def always_models(queue=nil, &block)
-    always(:async) do |collection|
-      collection.transformed(&block)
-    end
-  end
-
   def always_models(queue=nil, pager=nil, &block)
     always_canceller = always(:async) do |collection|
       collection.transformed(queue, pager, &block)
     end
-    pager_canceller = RMX(pager).on(:changed) do
-      changed!
+    pager_canceller = if pager
+      RMX(pager).on(:changed) do
+        changed!
+      end
     end
     off_block = proc do
       always_canceller.call
-      pager_canceller.call
+      if pager_canceller
+        pager_canceller.call
+      end
     end
     off_block
   end
 
-  # completes with `self` every time the collection :changed fires.
+  # completes with `models` every time the collection changes.
   # does not retain `self` or the sender.
+  # takes optional pager.
   # returns an "unbinder" that can be called to stop listening.
-  def changed(queue=nil, &block)
-    return false if cancelled?
-    RMX(self).on(:changed, :queue => queue, &block)
+  def changed_models(queue=nil, pager=nil, &block)
+    changed_canceller = changed(:async) do |collection|
+      collection.transformed(queue, pager, &block)
+    end
+    pager_canceller = if pager
+      RMX(pager).on(:changed) do
+        changed!
+      end
+    end
+    off_block = proc do
+      changed_canceller.call
+      if pager_canceller
+        pager_canceller.call
+      end
+    end
+    off_block
   end
 
-  # completes with `self`, `snap`, `prev` every time the collection :added fires.
+  # completes with `model` every time the collection :added_model fires.
   # does not retain `self` or the sender.
   # returns an "unbinder" that can be called to stop listening.
-  def added(queue=nil, &block)
-    return false if cancelled?
-    RMX(self).on(:added, :queue => queue, &block)
-  end
-
   def added_model(queue=nil, &block)
-    added(:async) do |collection, snap|
-      collection.transform(snap).once(&block)
+    RMX(self).on(:added_model, :queue => :async) do |model|
+      RMXFirebase.block_on_queue(queue, model, &block)
     end
   end
 
-  # completes with `self`, `snap` every time the collection :removed fires.
+  # completes with `model` every time the collection :removed fires.
   # does not retain `self` or the sender.
   # returns an "unbinder" that can be called to stop listening.
   def removed(queue=nil, &block)
-    return false if cancelled?
-    RMX(self).on(:removed, :queue => queue, &block)
+    RMX(self).on(:removed_model, :queue => :async) do |model|
+      RMXFirebase.block_on_queue(queue, model, &block)
+    end
   end
 
-  # completes with `self`, `snap`, `prev` every time the collection :moved fires.
-  # does not retain `self` or the sender.
-  # returns an "unbinder" that can be called to stop listening.
-  def moved(queue=nil, &block)
-    return false if cancelled?
-    RMX(self).on(:moved, :queue => queue, &block)
-  end
-
+  # internal
   def initialize(_ref)
-    @snaps_by_name = {}
     @snaps = []
-    @transformations_table = {}
-    @added_handler = nil
-    @removed_handler = nil
-    @moved_handler = nil
     @value_handler = nil
     @cancel_error = nil
     @ref = nil
-    @root = nil
+    @state = nil
     setup_ref(_ref)
   end
 
   # internal
   def setup_ref(_ref)
     _clear_current_ref!
-    @ref = _ref
-    @root = Firebase.alloc.initWithRepo(_ref.repo, andPath:_ref.path)
-    RMXFirebase::QUEUE.barrier_async do
+    RMX.synchronized do
+      @ref = _ref
       cancel_block = lambda do |err|
         @cancel_error = err
         cancelled!
       end
-      @added_handler = _ref.on(:added) do |snap, prev|
-        # p "NORMAL ", snap.name, prev
+      @value_handler = _ref.on(:value, { :disconnect => cancel_block }) do |collection|
         RMXFirebase::QUEUE.barrier_async do
-          # p "BARRIER", snap.name, prev
-          add(snap, prev)
+          @snaps = collection.children
+          ready!
         end
       end
-      @removed_handler = _ref.on(:removed) do |snap|
-        RMXFirebase::QUEUE.barrier_async do
-          remove(snap)
-        end
-      end
-      @moved_handler = _ref.on(:moved) do |snap, prev|
-        RMXFirebase::QUEUE.barrier_async do
-          add(snap, prev)
-        end
-      end
-      @value_handler = @root.on(:value, { :disconnect => cancel_block }) do |collection|
-        RMXFirebase::QUEUE.barrier_async do
-          ready! unless ready?
-        end
-      end
-      RMX(self).on(:cancelled, :exclusive => [ :ready, :changed, :added, :removed, :moved ], :queue => :async)
     end
   end
 
   def _clear_current_ref!
-    # p "_clear_current_ref"
-    if @added_handler
-      @ref.off(@added_handler)
-      @added_handler = nil
+    RMX.synchronized do
+      # p "_clear_current_ref"
+      if @value_handler
+        @ref.off(@value_handler)
+        @value_handler = nil
+      end
+      @ref = nil
+      @state = nil
     end
-    if @removed_handler
-      @ref.off(@removed_handler)
-      @removed_handler = nil
-    end
-    if @moved_handler
-      @ref.off(@moved_handler)
-      @moved_handler = nil
-    end
-    if @value_handler
-      @root.off(@value_handler)
-      @value_handler = nil
-    end
-    @ref = nil
-    @root = nil
-    @state = nil
   end
 
   # internal
@@ -274,114 +263,20 @@ class RMXFirebaseCollection
   def cancelled!
     RMXFirebase::QUEUE.barrier_async do
       @state = :cancelled
-      RMX(self).trigger(:cancelled, self)
-      RMX(self).trigger(:finished, self)
+      changed!
     end
   end
 
   # internal
   def changed!
     RMXFirebase::QUEUE.barrier_async do
-      RMX(self).trigger(:changed, self)
       if ready?
         RMX(self).trigger(:ready, self)
-        RMX(self).trigger(:finished, self)
       end
-    end
-  end
-
-  # internal
-  def store_transform(snap)
-    RMX(self).require_queue!(RMXFirebase::QUEUE, __FILE__, __LINE__) if RMX::DEBUG_QUEUES
-    @transformations_table[snap] ||= begin
-      model = transform(snap)
-      # RMX(model).on(:cancelled, :queue => :async) do
-      #   changed!
-      # end
-      model
-    end
-  end
-
-  def clear_transform(snap)
-    RMX(self).require_queue!(RMXFirebase::QUEUE, __FILE__, __LINE__) if RMX::DEBUG_QUEUES
-    @transformations_table.delete(snap)
-  end
-
-  def clear_transformations!
-    RMXFirebase::QUEUE.barrier_async do
-      @transformations_table.clear
-    end
-  end
-
-  # internal
-  def add(snap, prev)
-    RMX(self).require_queue!(RMXFirebase::QUEUE, __FILE__, __LINE__) if RMX::DEBUG_QUEUES
-    moved = false
-
-    if current_index = @snaps_by_name[snap.name]
-      if current_index == 0 && prev.nil?
-        return
-      elsif current_index > 0 && prev && @snaps_by_name[prev] == current_index - 1
-        return
+      if cancelled?
+        RMX(self).trigger(:cancelled, self)
       end
-      moved = true
-      @snaps.delete_at(current_index)
-      if was_index = @snaps_by_name.delete(snap.name)
-        @snaps_by_name.keys.each do |k|
-          v = @snaps_by_name[k]
-          if v > was_index
-            @snaps_by_name[k] -= 1
-          end
-        end
-      end
-      # raise if snaps_by_name.values.uniq.size != snaps_by_name.values.size
-    end
-    # raise if snaps_by_name.values.uniq.size != snaps_by_name.values.size
-    if prev && (index = @snaps_by_name[prev])
-      new_index = index + 1
-      @snaps.insert(new_index, snap)
-      @snaps_by_name.keys.each do |k|
-        v = @snaps_by_name[k]
-        if v >= new_index
-          @snaps_by_name[k] += 1
-        end
-      end
-      @snaps_by_name[snap.name] = new_index
-      # raise if snaps_by_name.values.uniq.size != snaps_by_name.values.size
-    else
-      @snaps.unshift(snap)
-      @snaps_by_name.keys.each do |k|
-        v = @snaps_by_name[k]
-        @snaps_by_name[k] += 1
-      end
-      @snaps_by_name[snap.name] = 0
-      # raise if snaps_by_name.values.uniq.size != snaps_by_name.values.size
-    end
-    if ready?
-      if moved
-        RMX(self).trigger(:moved, self, snap, prev)
-      else
-        RMX(self).trigger(:added, self, snap, prev)
-      end
-    end
-    changed!
-  end
-
-  # internal
-  def remove(snap)
-    if current_index = @snaps_by_name[snap.name]
-      clear_transform(snap)
-      @snaps.delete_at(current_index)
-      @snaps_by_name.keys.each do |k|
-        v = @snaps_by_name[k]
-        if v > current_index
-          @snaps_by_name[k] -= 1
-        end
-      end
-      if ready?
-        RMX(self).trigger(:removed, self, snap)
-      end
-      changed!
+      RMX(self).trigger(:changed, self)
     end
   end
 
