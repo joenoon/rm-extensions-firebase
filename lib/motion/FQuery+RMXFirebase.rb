720518c415448fd15fd0e44d3352db3df2e1a291
Joe Noon <joenoon@gmail.com>
2014-08-27 20:48:02 -0700

    first cut signals for firebase observations

--- lib/motion/FQuery+RMXFirebase.rb
+++ lib/motion/FQuery+RMXFirebase.rb
@@ -1,26 +1,6 @@
 class FQuery
 
-  EVENT_TYPES_MAP = {
-    :child_added => FEventTypeChildAdded,
-    :added => FEventTypeChildAdded,
-    :child_moved => FEventTypeChildMoved,
-    :moved => FEventTypeChildMoved,
-    :child_changed => FEventTypeChildChanged,
-    :changed => FEventTypeChildChanged,
-    :child_removed => FEventTypeChildRemoved,
-    :removed => FEventTypeChildRemoved,
-    :value => FEventTypeValue
-  }
-
-  #                                  A#  ONCE?  DISC?
-  TYPE_ONE_ARG_ONCE_DO_DISCONNECT = [ 1, true,  true  ]
-  TYPE_ONE_ARG_ONCE_NO_DISCONNECT = [ 1, true,  false ]
-  TYPE_ONE_ARG_CONT_DO_DISCONNECT = [ 1, false, true  ]
-  TYPE_ONE_ARG_CONT_NO_DISCONNECT = [ 1, false, false ]
-  TYPE_TWO_ARG_ONCE_DO_DISCONNECT = [ 2, true,  true  ]
-  TYPE_TWO_ARG_ONCE_NO_DISCONNECT = [ 2, true,  false ]
-  TYPE_TWO_ARG_CONT_DO_DISCONNECT = [ 2, false, true  ]
-  TYPE_TWO_ARG_CONT_NO_DISCONNECT = [ 2, false, false ]
+  RECURSIVE_LOCK = NSRecursiveLock.new
 
   def rmx_object_desc
     "#{super}:#{description}"
@@ -38,81 +18,208 @@ class FQuery
     queryEndingAtPriority(priority)
   end
 
-  def on(_event_type, options={}, &and_then)
-
-    event_type = EVENT_TYPES_MAP[_event_type]
-    raise "event handler is unknown: #{_event_type.inspect}" unless event_type
-
-    and_then ||= options[:completion]
-    raise "event handler is required" unless and_then
-    raise "event handler must accept one or two arguments" unless and_then.arity == 1 || and_then.arity == 2
-
-    dealloc_signals = [ rac_willDeallocSignal, and_then.rac_willDeallocSignal ]
-
-    if and_disconnect = options[:disconnect]
-      raise ":disconnect handler must not accept any arguments" if and_disconnect && and_disconnect.arity != 1
-      dealloc_signals << and_disconnect.owner.rac_willDeallocSignal
-    end
+  def on(event_type, options={}, &and_then)
+    completion = and_then || options[:completion]
+    disconnect = options[:disconnect]
 
     once = options[:once]
 
-    completion = RMX.safe_lambda(and_then, "completion block for #{_event_type.inspect}")
-
-    disconnect = nil
-    if and_disconnect
-      disconnect = RMX.safe_lambda(and_disconnect, "disconnect block for #{_event_type.inspect}")
-    end
-
-    handler = nil
-    RMX.synchronized do
-      handler = case [ completion.arity, !!once, !!disconnect ]
-      when TYPE_ONE_ARG_ONCE_DO_DISCONNECT
-        observeSingleEventOfType(event_type, withBlock:completion, withCancelBlock:disconnect)
-      when TYPE_ONE_ARG_CONT_DO_DISCONNECT
-        observeEventType(event_type, withBlock:completion, withCancelBlock:disconnect)
-      when TYPE_ONE_ARG_ONCE_NO_DISCONNECT
-        observeSingleEventOfType(event_type, withBlock:completion)
-      when TYPE_ONE_ARG_CONT_NO_DISCONNECT
-        observeEventType(event_type, withBlock:completion)
-      when TYPE_TWO_ARG_ONCE_DO_DISCONNECT
-        observeSingleEventOfType(event_type, andPreviousSiblingNameWithBlock:completion, withCancelBlock:disconnect)
-      when TYPE_TWO_ARG_CONT_DO_DISCONNECT
-        observeEventType(event_type, andPreviousSiblingNameWithBlock:completion, withCancelBlock:disconnect)
-      when TYPE_TWO_ARG_ONCE_NO_DISCONNECT
-        observeSingleEventOfType(event_type, andPreviousSiblingNameWithBlock:completion)
-      when TYPE_TWO_ARG_CONT_NO_DISCONNECT
-        observeEventType(event_type, andPreviousSiblingNameWithBlock:completion)
+    sig = case event_type
+    when :child_added, :added
+      if once
+        rac_addedOnceSignal
+      else
+        rac_addedSignal
       end
-
-      @active_handlers ||= []
-      @active_handlers << handler
+    when :child_moved, :moved
+      if once
+        rac_movedOnceSignal
+      else
+        rac_movedSignal
+      end
+    when :child_changed, :changed
+      if once
+        rac_changedOnceSignal
+      else
+        rac_changedSignal
+      end
+    when :child_removed, :removed
+      if once
+        rac_removedOnceSignal
+      else
+        rac_removedSignal
+      end
+    when :value
+      if once
+        rac_valueOnceSignal
+      else
+        rac_valueSignal
+      end
     end
 
-    RACSignal.zip(dealloc_signals.uniq).take(1).subscribeCompleted(-> {
-      off(handler)
-    })
+    dis = nil
 
-    handler
+    if completion
+      safe_completion = RMX.safe_lambda(completion, "completion block for #{event_type.inspect}")
+      sig = sig.takeUntil(completion.owner.rac_willDeallocSignal)
+      if disconnect
+        safe_disconnect = RMX.safe_lambda(disconnect, "disconnect block for #{event_type.inspect}")
+        dis = sig.subscribeNext(safe_completion, error:safe_disconnect)
+      else
+        dis = sig.subscribeNext(safe_completion)
+      end
+    end
+
+    dis
   end
 
   def once(event_type, options={}, &and_then)
     on(event_type, options.merge(:once => true), &and_then)
   end
 
-  def off(handler)
-    RMX.synchronized do
-      if @active_handlers
-        if @active_handlers.delete(handler)
-          removeObserverWithHandle(handler)
-          # p("remove handle#{handler}")
-          true
-        end
-      end
-    end
-  end
-
   def description
     AFHTTPRequestSerializer.serializer.requestWithMethod("GET", URLString: "https://#{repo.repoInfo.host}#{path.toString}", parameters:queryParams.queryObject).URL.absoluteString
+  end
+
+  # Value sends curr
+  def rac_valueSignal
+    RECURSIVE_LOCK.lock
+    @_rac_valueSignal_autoreconnector = recon ||= {}
+    if pub = recon[:publisher] and pub.serialDisposable.isDisposed.boolValue
+      # NSLog("sig dead, clear")
+      @_rac_valueSignal_autoreconnector.clear
+    end
+    recon[:signal] = signal ||= RACSignal.createSignal(-> (subscriber) {
+      RECURSIVE_LOCK.lock
+      handler = observeEventType(FEventTypeValue, withBlock:->(curr) {
+        subscriber.sendNext(curr)
+      }, withCancelBlock:->(err) {
+        subscriber.sendError(err)
+      })
+      # NSLog("createSignal #{description} handler: #{handler}")
+
+      dis = RACDisposable.disposableWithBlock(-> {
+        RECURSIVE_LOCK.lock
+        # NSLog("disposableWithBlock removeObserverWithHandle(#{handler})")
+        removeObserverWithHandle(handler)
+        RECURSIVE_LOCK.unlock
+      })
+      RECURSIVE_LOCK.unlock
+      dis
+    }.weak!)
+    recon[:publisher] = publisher ||= signal.multicast(RACReplaySubject.replaySubjectWithCapacity(1))
+    recon[:connection] = connection ||= publisher.autoconnect
+    RECURSIVE_LOCK.unlock
+    connection
+  end
+
+  # ChildAdded sends [ curr, prev ]
+  def rac_addedSignal
+    RACSignal.createSignal(-> (subscriber) {
+      RECURSIVE_LOCK.lock
+      handler = observeEventType(FEventTypeChildAdded, andPreviousSiblingWithBlock:->(curr, prev) {
+        subscriber.sendNext([ curr, prev ])
+      }, withCancelBlock:->(err) {
+        subscriber.sendError(err)
+      })
+      # NSLog("rac_addedSignal #{description} handler: #{handler}")
+
+      dis = RACDisposable.disposableWithBlock(-> {
+        RECURSIVE_LOCK.lock
+        # NSLog("rac_addedSignal disposableWithBlock removeObserverWithHandle(#{handler})")
+        removeObserverWithHandle(handler)
+        RECURSIVE_LOCK.unlock
+      })
+      RECURSIVE_LOCK.unlock
+      dis
+    }.weak!)
+  end
+
+  # ChildMoved sends [ curr, prev ]
+  def rac_movedSignal
+    RACSignal.createSignal(-> (subscriber) {
+      RECURSIVE_LOCK.lock
+      handler = observeEventType(FEventTypeChildMoved, andPreviousSiblingWithBlock:->(curr, prev) {
+        subscriber.sendNext([ curr, prev ])
+      }, withCancelBlock:->(err) {
+        subscriber.sendError(err)
+      })
+      # NSLog("rac_movedSignal #{description} handler: #{handler}")
+
+      dis = RACDisposable.disposableWithBlock(-> {
+        RECURSIVE_LOCK.lock
+        # NSLog("rac_movedSignal disposableWithBlock removeObserverWithHandle(#{handler})")
+        removeObserverWithHandle(handler)
+        RECURSIVE_LOCK.unlock
+      })
+      RECURSIVE_LOCK.unlock
+      dis
+    }.weak!)
+  end
+
+  # ChildChanged sends [ curr, prev ]
+  def rac_changedSignal
+    RACSignal.createSignal(-> (subscriber) {
+      RECURSIVE_LOCK.lock
+      handler = observeEventType(FEventTypeChildChanged, andPreviousSiblingWithBlock:->(curr, prev) {
+        subscriber.sendNext([ curr, prev ])
+      }, withCancelBlock:->(err) {
+        subscriber.sendError(err)
+      })
+      # NSLog("rac_changedSignal #{description} handler: #{handler}")
+
+      dis = RACDisposable.disposableWithBlock(-> {
+        RECURSIVE_LOCK.lock
+        # NSLog("rac_changedSignal disposableWithBlock removeObserverWithHandle(#{handler})")
+        removeObserverWithHandle(handler)
+        RECURSIVE_LOCK.unlock
+      })
+      RECURSIVE_LOCK.unlock
+      dis
+    }.weak!)
+  end
+
+  # ChildRemoved sends curr
+  def rac_removedSignal
+    RACSignal.createSignal(-> (subscriber) {
+      RECURSIVE_LOCK.lock
+      handler = observeEventType(FEventTypeChildRemoved, withBlock:->(curr) {
+        subscriber.sendNext(curr)
+      }, withCancelBlock:->(err) {
+        subscriber.sendError(err)
+      })
+      # NSLog("rac_removedSignal #{description} handler: #{handler}")
+
+      dis = RACDisposable.disposableWithBlock(-> {
+        RECURSIVE_LOCK.lock
+        # NSLog("rac_removedSignal disposableWithBlock removeObserverWithHandle(#{handler})")
+        removeObserverWithHandle(handler)
+        RECURSIVE_LOCK.unlock
+      })
+      RECURSIVE_LOCK.unlock
+      dis
+    }.weak!)
+  end
+
+  # once signals
+
+  def rac_valueOnceSignal
+    rac_valueSignal.take(1)
+  end
+
+  def rac_addedOnceSignal
+    rac_addedSignal.take(1)
+  end
+
+  def rac_movedOnceSignal
+    rac_movedSignal.take(1)
+  end
+
+  def rac_changedOnceSignal
+    rac_changedSignal.take(1)
+  end
+  def rac_removedOnceSignal
+    rac_removedSignal.take(1)
   end
 
 end
