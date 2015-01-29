002fc4588eb8414e5d32493ead44a3a669199e23
Joe Noon <joenoon@gmail.com>
2015-01-29 13:21:22 -0800

    switch back to sequential for now

--- lib/motion/RMXFirebase.rb
+++ lib/motion/RMXFirebase.rb
@@ -12,8 +12,14 @@ module RMXFirebase
     all.map(->(x) { x.flatten })
   end
 
+  def self.allNextOrCompletedSequential(signals)
+    groups = signals.each_slice(100).to_a.map { |x| x.size > 0 ? RACSignal.concat(x).collect.rmx_expandTuple : RACSignal.return([]) }
+    all = groups.size > 0 ? RACSignal.concat(groups).collect.rmx_expandTuple : RACSignal.return([])
+    all.map(->(x) { x.flatten })
+  end
+
   def self.batchSignal(*models)
-    allNextOrCompleted(models.flatten.compact.map(&:strongOnceSignal))
+    allNextOrCompletedSequential(models.flatten.compact.map(&:strongOnceSignal))
   end
 
   def self.batchMainSignal(*models)
