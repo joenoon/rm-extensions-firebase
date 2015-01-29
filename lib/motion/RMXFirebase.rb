9bd39668e54a51cd28eae5cf443e08a6796c5fda
Joe Noon <joenoon@gmail.com>
2015-01-29 00:27:58 -0800

    update how batching is implemented

--- lib/motion/RMXFirebase.rb
+++ lib/motion/RMXFirebase.rb
@@ -1,7 +1,19 @@
 module RMXFirebase
 
+  def self.allNextOrCompleted(signals)
+    groups = signals.each_slice(100).to_a.map { |x| x.size > 0 ? RACSignal.combineLatest(x).take(1).rmx_expandTuple : RACSignal.return([]) }
+    all = groups.size > 0 ? RACSignal.combineLatest(groups).take(1).rmx_expandTuple : RACSignal.return([])
+    all.map(->(x) { x.flatten })
+  end
+
+  def self.allNextOrCompletedSequential(signals, jitter:jitter)
+    groups = signals.each_slice(100).to_a.map { |x| x.size > 0 ? RACSignal.concat(x).collect.rmx_expandTuple.delay(jitter) : RACSignal.return([]) }
+    all = groups.size > 0 ? RACSignal.concat(groups).collect.rmx_expandTuple : RACSignal.return([])
+    all.map(->(x) { x.flatten })
+  end
+
   def self.batchSignal(*models)
-    RACSignal.concat(models.flatten.compact.map(&:strongOnceSignal)).collect
+    allNextOrCompleted(models.flatten.compact.map(&:strongOnceSignal))
   end
 
   def self.batchMainSignal(*models)
