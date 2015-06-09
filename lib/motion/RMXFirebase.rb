bbcd8e737d982eed19d23ee8398b362dcff70a94
Joe Noon <joenoon@gmail.com>
2015-06-08 18:46:55 -0700

    attempt batchSignal improvement

--- lib/motion/RMXFirebase.rb
+++ lib/motion/RMXFirebase.rb
@@ -1,25 +1,15 @@
 module RMXFirebase
 
-  def self.allNextOrCompleted(signals)
-    groups = signals.each_slice(100).to_a.map { |x| x.size > 0 ? RACSignal.combineLatest(x).take(1).rmx_expandTuple : RACSignal.return([]) }
-    all = groups.size > 0 ? RACSignal.combineLatest(groups).take(1).rmx_expandTuple : RACSignal.return([])
-    all.map(->(x) { x.flatten })
-  end
-
-  def self.allNextOrCompletedSequential(signals, jitter:jitter)
-    groups = signals.each_slice(100).to_a.map { |x| x.size > 0 ? RACSignal.concat(x).collect.rmx_expandTuple.delay(jitter) : RACSignal.return([]) }
-    all = groups.size > 0 ? RACSignal.concat(groups).collect.rmx_expandTuple : RACSignal.return([])
-    all.map(->(x) { x.flatten })
-  end
-
-  def self.allNextOrCompletedSequential(signals)
-    groups = signals.each_slice(100).to_a.map { |x| x.size > 0 ? RACSignal.concat(x).collect.rmx_expandTuple : RACSignal.return([]) }
-    all = groups.size > 0 ? RACSignal.concat(groups).collect.rmx_expandTuple : RACSignal.return([])
-    all.map(->(x) { x.flatten })
-  end
-
   def self.batchSignal(*models)
-    allNextOrCompletedSequential(models.flatten.compact.map(&:strongOnceSignal))
+    ms = models.flatten.compact
+    if ms.size == 0
+      RACSignal.return([])
+    else
+      sigs = ms.map(&:strongOnceSignal)
+      RACSignal.merge(sigs)
+      .ignoreValues
+      .concat(RACSignal.return(ms))
+    end
   end
 
   def self.batchMainSignal(*models)
